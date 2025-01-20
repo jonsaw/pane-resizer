e0465f319bdacb07d0f5e1ae7e90a469daab647a
Jon Saw <jon.saw@gmail.com>
2025-01-20 19:50:41 +0800

    Setup zed editor

---
+++ .zed/format.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+leptosfmt --stdin | \
+    rustywind --stdin --custom-regex "\b(?:class(?:Name)*\s*=\s*[\"'])([_a-zA-Z0-9\.\s\-:\[\]/]+)[\"']" --output-css-file "$(pwd)/styles/styles.css" | \
+    rustfmt --edition 2021
