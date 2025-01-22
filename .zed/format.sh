658a60d8c10ea81adcd19a8d161180bfbd3047d8
Jon Saw <jon.saw@gmail.com>
2025-01-22 10:02:23 +0800

    Ignore compiled distributed styles

--- .zed/format.sh
+++ .zed/format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 leptosfmt --stdin | \
-    rustywind --stdin --custom-regex "\b(?:class(?:Name)*\s*=\s*[\"'])([_a-zA-Z0-9\.\s\-:\[\]/]+)[\"']" --output-css-file "$(pwd)/styles/styles.css" | \
+    rustywind --stdin --custom-regex "\b(?:class(?:Name)*\s*=\s*[\"'])([_a-zA-Z0-9\.\s\-:\[\]/]+)[\"']" --output-css-file "$(pwd)/diststyles/styles.css" | \
     rustfmt --edition 2021
